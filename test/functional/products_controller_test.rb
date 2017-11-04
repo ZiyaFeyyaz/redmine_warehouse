bd046f5dfee4d44c391fbcc54d7b17ca74737002
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2017-11-04 19:08:53 +0300

    Add broken tests

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,8 +1,43 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ProductsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  setup do
+    @product = products(:one)
+  end
+
+
+  test 'should get index' do
+    get products_url
+    assert_response :success
+  end
+
+  test 'should create product' do
+    product_params = {
+      product: {
+        :name => 'One',
+        :price => 9.9,
+        :quantity => 5,
+        :issue_id => 1,
+      }
+    }
+
+    assert_difference('Product.count') do
+      post product_url, params: product_params
+    end
+
+    assert_redirected_to product_path(Product.last)
+  end
+
+  test 'should show product' do
+    get article_url(@product)
+    assert_response :success
+  end
+
+  test 'should destroy product' do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to product_path
   end
 end
